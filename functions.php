fe4eb6a20a347909dae43409c6f96bb08dc7558c
t638403 <e.j.vd.hooven@gmail.com>
2017-03-03 19:24:32 +0100

    Added comments and added template function

--- functions.php
+++ functions.php
@@ -1,4 +1,21 @@
 <?php
-function h($string) {
+
+/**
+ * Escape characters that have meaning in HTML
+ *
+ * @param $string
+ * @return string
+ */
+function h($string)
+{
 	return htmlspecialchars($string, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+}
+
+function t($templateName, $templateVars = [])
+{
+	$templateFile = dirname(__DIR__) . "/jowan/template/$templateName.php";
+	extract($templateVars);
+	ob_start();
+	include $templateFile;
+	return ob_get_clean();
 }
