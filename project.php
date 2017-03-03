de246cb849aef15a34b8f4b9d9b8e85ae58e3ed7
t638403 <e.j.vd.hooven@gmail.com>
2017-03-03 17:17:04 +0100

    A little protection

--- project.php
+++ project.php
@@ -1,5 +1,23 @@
-<div class="project <?php echo $_GET['name']?>">
+<div class="project <?php echo h($_GET['name'])?>">
     <div class="container">
-        <?php include(__DIR__ . '/projects/' .  $_GET['name'] . '/index.php'); ?>
+
+        <?php
+			$dirs = scandir(__DIR__ . '/projects/');
+			$foundProject = false;
+			foreach($dirs as $dir)
+			{
+				if($dir == $_GET['name'])
+				{
+					$foundProject = true;
+					break;
+				}
+			}
+		?>
+
+		<?php if($foundProject): ?>
+			<?php include(__DIR__ . '/projects/' .  $_GET['name'] . '/index.php'); ?>
+		<?php else: ?>
+			<p>Project &quot;<em><?= h($_GET['name']) ?></em>&quot; niet gevonden.</p>
+		<?php endif; ?>
     </div>
 </div>
